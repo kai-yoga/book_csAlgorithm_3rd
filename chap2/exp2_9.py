f4cacb04bb53860134019997162961fe5ff45b35
kai-y <1532398723@qq.com>
2021-07-05 10:16:37 +0800

    --edit some base info such as sumary,base file and so on

--- chap2/exp2_9.py
+++ chap2/exp2_9.py
@@ -1,55 +1,4 @@
-# from chap2 import  exp2_8 as quickSort
-# import math
-# L=quickSort.input_L
-#
-# def linerSearch(L,left,right,k):
-#     if right-left<=75:
-#         return quickSort.quickSort(L)[left+k-1]
-#     else:
-#         # left=0
-#         # right=len(L)-1
-#         groups=math.ceil((right-left)/5)
-#         print(L)
-#         res=[]
-#         for g in range(groups):
-#             ####将每组排序
-#             # print(quickSort.quickSort(L[left + g * 5:left + (g + 1) * 5]))
-#             # print('g=>',g,'left=>',left+g*5,'right=>',left+(g+1)*5,
-#             #       'mid',min(left+g*5+2,right)
-#             #       )
-#             # print(quickSort.quickSort(L[left + g * 5:left + (g + 1) * 5]))
-#             # L[min(2, right - left - g * 5)]=L[left+g*5]
-#             # tmp=quickSort.quickSort(L[left + g * 5:left + (g + 1) * 5])[min(2,right-left-g*5)]
-#             # L[min(2, right - left - g * 5)]=L[left+g*5]
-#             # L[left + g * 5]=tmp
-#             L[left+g*5:min(right,left+(g+1)*5)]=quickSort.quickSort(L[left + g * 5:min(right,left+(g+1)*5)])
-#             tmp=L[min(2, right - left - g * 5)+left+g*5]
-#             L[min(2, right - left - g * 5)+left+g*5]=L[left+g*5]
-#             L[left + g * 5]=tmp
-#
-#
-#             # L[min(2,right-left-g*5)]=tmp
-#             res.append({
-#                 'value':quickSort.quickSort(L[left + g * 5:left + (g + 1) * 5])[min(2,right-left-g*5)],
-#                 'pos':left+5*g
-#             })
-#         media=linerSearch(L,left,)
-#         # media_list=[key for key in res]
-#         # media=linerSearch(media_list,k//2)
-#
-#         print(res)
-#         print("quickSort=>",L)
-#
-#     # res=res+quickSort.quickSort(L[left + g * 5:left + (g + 1) * 5])[min(left+g*5+2,right)]
-#           # 'media=>',math.ceil((min(right,left+(g+1)*5)-(left+g*5))/5)+left+g*5
-#     # print(
-#     #     math.ceil(
-#     #         (min(right,left+(g+1)*5)-left*g)/5+left*g
-#     #     )
-#     # )
-#     # res=res+quickSort.quickSort(L[left+g*5:left+(g+1)*5])[math.ceil((min(right,left+(g+1)*5)-left*g)/5)]
-#
-#     # print('res=>',res)
+
 
 from chap2 import exp2_8 as quicksort, base
 import math
@@ -80,7 +29,7 @@ def mediaPartition(L, media):
 
 # print(base.CONST_L)
 # print(mediaPartition(base.CONST_L,0,len(base.CONST_L)-1,13))
-def linerSearche(L, left, right, k):
+def linerSearch(L, left, right, k):
     '''
     寻找L中第k小元素
     :param L: 输入列表
@@ -89,7 +38,7 @@ def linerSearche(L, left, right, k):
     :param k: 第k小
     :return: 第k小元素
     '''
-    if right - left < 3:
+    if right - left < 75:
         '''对于长度小于75的列表，直接排序，输出其中的第k小元素即可'''
         L[left:right] = bubleSort.buble(L[left:right])
         # print(L)
@@ -107,29 +56,19 @@ def linerSearche(L, left, right, k):
             L[left + 5 * g + (min(right, left + (g + 1) * 5) - left - 5 * g) // 2] = L[left + g]
             L[left + g] = tmp
         # print('before media', 'left', left, 'groups', groups, 'media_media', groups // 2, 'L', L)
-        media = linerSearche(L, left, left + groups, groups // 2)
+        media = linerSearch(L, left, left + groups, groups // 2)
         # print('left',left,'right',right)
         t = mediaPartition(L, media)
         L = t[0]
         media_pos = t[1]  ###对L进行，找到中位数的中位数位置，并划分
         # print('after find media', media, media_pos, L)
         if media_pos - left + 1 >= k:
-            return linerSearche(L, left, media_pos, k)
+            return linerSearch(L, left, media_pos, k)
         else:
-            return linerSearche(L, media_pos + 1, right, k - media_pos)
-        # print(L)
-        # return L
-        # print(L)
+            return linerSearch(L, media_pos + 1, right, k - media_pos)
 
 
-# print(quicksort.quickSort(base.CONST_L))
-# print(quicksort.quickSort())
-# print(quicksort.quickSort(base.CONST_L))
-# for k in range(12):
-# x = linerSearche([13, 16, 9, 19, 15, 18, 18, 14, 13, 20, 35, 100], 0, right=11, k=10)
-# # print('k=>',k,'x=>',x)
-# print(x)
 
 for k in range(12):
-    x = linerSearche([13, 16, 9, 19, 15, 18, 18, 14, 13, 20, 35, 100], 0, right=11, k=k)
+    x = linerSearch([13, 16, 9, 19, 15, 18, 18, 14, 13, 20, 35, 100], 0, right=11, k=k)
     print('k=>',k,'x=>',x)
